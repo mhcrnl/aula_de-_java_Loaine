f8500188426b84185aca1165549ab723bdf5bcdd
Francisco_Junio <juniothai7@gmail.com>
2015-07-23 21:48:42 -0300

    Iniciando com variaveis
    Nocão do que variavel e seu uso na programção.Exemplos de uso de
    variaveis na vida ,como quando utilizamos qualquer arquivo no PC, ele
    pode ser considerado uma variavel.

---
+++ java_aula10/variaveis.java
@@ -0,0 +1,14 @@
+package junio_pc.curso.java_aula10;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		int idade = 52;
+		String nome = "jose";
+		String nomeDoMeuCachorro = "dog";
+
+		System.out.println("O meu nome é " + nome + " e minha idade e  " + idade + " o meu cachorro se chama "
+				+ nomeDoMeuCachorro);
+	}
+
+}
