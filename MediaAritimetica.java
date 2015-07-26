88b4a3ab3588a0fd5254fc29cbde216872de4b16
Francisco_Junio <juniothai7@gmail.com>
2015-07-26 16:55:33 -0300

    Media Aritimetica

---
+++ MediaAritimetica.java
@@ -0,0 +1,27 @@
+package com.junio_pc.curso_java_aula13.labs;
+
+import java.util.Scanner;
+
+public class MediaAritimetica {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Entre com a primeira nota");
+		double nota1 = teclado.nextDouble();
+
+		System.out.println("Entre com a segunda nota");
+		double nota2 = teclado.nextDouble();
+
+		System.out.println("Entre com a terceira nota");
+		double nota3 = teclado.nextDouble();
+
+		System.out.println("Entre com a quarta nota");
+		double nota4 = teclado.nextDouble();
+		
+		double media = (nota1 + nota2 + nota3 + nota4);
+		System.out.println("A média foi : "+ media);
+
+	}
+
+}
