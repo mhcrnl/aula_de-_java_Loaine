a1f348daa67b11337fbd6be075a55106135ae606
Francisco_Junio <juniothai7@gmail.com>
2015-07-26 18:14:46 -0300

    convesor de metros para centrimentos

--- MetroParaCentrimento.java
+++ MetroParaCentrimento.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class MetroParaCentrimento {
 
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner (System.in);
-		
+		Scanner teclado = new Scanner(System.in);
+
 		System.out.println("Entre com a quantidade de metros");
 		double metros = teclado.nextDouble();
-		
-		//1m = 100cm
-		
+
+		// 1m = 100cm
+
 		double cm = metros * 100;
-		
-		System.out.println(metros + " m é igual a "+ cm +"cm");
-		
+
+		System.out.println(metros + " m é igual a " + cm + "cm");
+
 	}
 
 }
