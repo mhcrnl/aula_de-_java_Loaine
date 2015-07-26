cc3b5413714c656150597d49f2df8f02b3af32ed
Francisco_Junio <juniothai7@gmail.com>
2015-07-26 11:41:59 -0300

    curiosidade java
    No java e como uma roleta, quando somamos o valor maximo da variaveis
    ex:int, long, char ele retorna ao valor negativo
    -2147483648
    2147483647

---
+++ curso_java_aula11/CuriosidadeInt.java
@@ -0,0 +1,20 @@
+package com.junio_pc.curso_java_aula11;
+
+public class CuriosidadeInt {
+
+	public static void main(String[] args) {
+
+		int var1 = 2147483647;
+		int var2 = 100;
+
+		System.out.println(var1 + var2);
+
+		/*
+		 * No java e como uma roleta, quando somamos o valor maximo da variaveis
+		 * ex:int, long, char ele retorna ao valor negativo 
+		 * -2147483648
+		 * 2147483647
+		 */
+	}
+
+}
