9f501b52d26efa0f2eafc22b208684c6e94f123a
Francisco_Junio <juniothai7@gmail.com>
2015-07-26 12:00:52 -0300

    Sequencia de escape

---
+++ curso_java_aula11/SequenciaDeEscape.java
@@ -0,0 +1,12 @@
+package com.junio_pc.curso_java_aula11;
+
+public class SequenciaDeEscape {
+
+	public static void main(String[] args) {
+		//"Hello, World!"
+		System.out.print("\"Hello World\"\n\r");
+		// 1\4
+		System.out.println("1\\4");
+	}
+
+}
