348885bc5de1b5284d0c33b455845ae69a5733af
Francisco_Junio <juniothai7@gmail.com>
2015-07-26 11:40:14 -0300

    variaveis

--- java_aula10/variaveis.java
+++ java_aula10/variaveis.java
@@ -4,11 +4,20 @@ public class variaveis {
 
 	public static void main(String[] args) {
 		int idade = 52;
+		char o =111;
+		char i =105;
+		char interrogacao = 0x00E1;
+		boolean verdadeiro = true;
+		boolean falso = false;
 		String nome = "jose";
 		String nomeDoMeuCachorro = "dog";
 
 		System.out.println("O meu nome é " + nome + " e minha idade e  " + idade + " o meu cachorro se chama "
 				+ nomeDoMeuCachorro);
-	}
+		System.out.println(" "+ o + i + interrogacao+ " "+verdadeiro + " "+falso)
+;
+		//utilizar aspas para juntar 
+		
+		}
 
 }
